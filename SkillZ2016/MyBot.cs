549a942c9fadeae92a068e7da1b495d4fdfabda5
Harry Zalessky <HarryZalessky@hotmail.com>
2016-12-25 10:37:50 +0200

    Empty Project C#

---
+++ SkillZ2016/MyBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirates;
+
+namespace SkillZ2016
+{
+    public class MyBot : IPirateBot
+    {
+        public void DoTurn(PirateGame game)
+        {
+
+        }
+    }
+}
